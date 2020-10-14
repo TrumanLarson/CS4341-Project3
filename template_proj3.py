758980ac3e009f7c248d7e8bcd6dbdb1d33434bc
JPoulos000 <72040916+JPoulos000@users.noreply.github.com>
2020-10-14 16:03:49 -0400

    Added Confusion matrix and associated functions

--- template_proj3.py
+++ template_proj3.py
@@ -30,8 +30,76 @@ def main(argv):
 
     # Report Results
 
-    print(history.history)
-    model.predict()
+#create the matrix
+confusionMatrix = []
+for i in range(0,10):
+    confusionMatrix.insert(0, [0,0,0,0,0,0,0,0,0,0])
+
+print(history.history)
+#modified
+col = model.predict()
+row = trueval()
+
+confusionMatrix[row][col] += 1
+
+
+
+#functions for analyzing success of the model
+def modelAccuracy(matrix):
+    """
+    Return the accuracy of the model based on the confusion matrix.  
+    
+    Parameters
+    ----------
+    matrix : list of lists
+        The confusion matrix for the model
+    """   
+    correct = 0
+    total = 0
+    for i in range(0,10):
+        correct += matrix[i][i]
+        for j in range(0,10):
+            total += matrix[i][j]
+    return (correct/total)
+
+
+def modelPrecision(matrix, predictedVal):
+    """
+    Return the precision of the model for a particular value based on the confusion matrix.  
+    
+    Parameters
+    ----------
+    matrix : list of lists
+        The confusion matrix for the model
+    predictedVal: integer 0 to 9
+        The value predicted by the model
+    """   
+    correct = matrix[predictedVal][predictedVal]
+    total = 0
+    for i in range(0,10):
+        total += matrix[i][predictedVal]
+        
+    return (correct/total)
+
+
+def modelRecall(matrix, trueVal):
+    """
+    Return the recall of the model for a particular value based on the confusion matrix.  
+    
+    Parameters
+    ----------
+    matrix : list of lists
+        The confusion matrix for the model
+    trueVal: integer 0 to 9
+        The true value of the test by the model
+    """   
+    correct = matrix[trueVal][trueVal]
+    total = 0
+    for i in range(0,10):
+        total += matrix[trueVal][i]
+        
+    return (correct/total)
+
 
 if __name__ == "__main__":
     main(sys.argv)
